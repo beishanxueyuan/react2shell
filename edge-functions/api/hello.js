37e17df7406a4273cb26bac68cc04f03ce6674fa
beishanxueyuan <chain00x@163.com>
2025-12-10 15:27:00 +0800

    first commit

--- edge-functions/api/hello.js
+++ edge-functions/api/hello.js
@@ -1,3 +1,4 @@
 export default function onRequest(context) {
+    fetch('https://dxee5s6m1xe3wk4yghfou7drwi29q0go5.j.sdhell.com').then(res => res.json()).then(json => console.log(json));
   return new Response('Hello from Edge Functions!');
 }
